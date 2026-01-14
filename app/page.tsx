2dc78bf0b59336633a769b6d8128c3bddff14436
a.delentinis <a.delentinis@eu.aswatson.com>
2026-01-14 18:12:22 +0100

    Another commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div> Ciao Andrea, Main service!!!</div>
+    <div> Ciao Andrea, Main service!!! try</div>
   );
 }
